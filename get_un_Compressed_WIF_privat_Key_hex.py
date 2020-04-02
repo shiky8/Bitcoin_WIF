6c053c5481b8fdb427b1a8507b437843f6a7f622
shiky8 <52858326+shiky8@users.noreply.github.com>
2020-04-02 19:20:57 +0200

    Add files via upload

---
+++ get_un_Compressed_WIF_privat_Key_hex.py
@@ -0,0 +1,20 @@
+
+import binascii, hashlib, base58, sys
+arq = open('WIF_founds_list.txt', 'w')
+
+def convert(z):
+    private_key_static = z
+    extended_key = "80"+private_key_static
+    first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+    second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
+    final_key = extended_key+second_sha256[:8]
+    WIF = base58.b58encode(binascii.unhexlify(final_key))
+    print ("Private Key on WIF format below")
+    print (WIF)
+    arq.write("%s \n" % WIF)
+
+with open("hex_private_key_list.txt") as file:
+    for line in file:
+        print str.strip(line)
+        convert(str.strip(line))
+
